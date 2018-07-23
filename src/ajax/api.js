990a43aeab00415c1acfb0027787abf2ab0ab9d1
zhangyongwang <765298337@qq.com>
2018-07-23 15:41:57 +0800

    append the sign to request

--- src/ajax/api.js
+++ src/ajax/api.js
@@ -1,10 +1,54 @@
 import axios from 'axios'
 import qs from 'qs'
+import m from '../md5.min'
+
+function encryptparam(obj) {
+  var keys = [];
+  typeof (obj) == "object" ? "" : obj = {};
+
+  obj.app_key = '735F59558122F70A35670715E8D27055'//需要传的app_key
+  for (var key in obj) {
+    keys.push(key);
+  }
+  keys = keys.sort();
+  var sortData = {}
+
+  var strSign = "";
+  if (keys.length > 0) {
+    for (var k in keys) {
+      if (keys[k] == 'sign') {
+        continue;
+      }
+      if (typeof (keys[k]) == "undefined") {
+        continue;
+      }
+      strSign += "&" + keys[k] + "=" + encodeURIComponent(obj[keys[k]]);
+    }
+  }
+  var appkey = "K1vX5aeNEMdnOctoUIzZpLDJXMf3IfPAv8TRNcJjmndq2TKisdX09FWr7";
+  strSign = strSign.substr(1);
+  console.log(strSign + appkey)
+  obj['sign'] = m.md5(strSign + appkey).toLowerCase();
+  return obj
+}
+
+function toSign(obj) {
+  obj.app_key = '735F59558122F70A35670715E8D27055'//需要传的app_key
+  let objArray = []
+  for (var i in obj){
+    objArray.push(obj[i])
+  }
+  objArray.sort()
+  let sign = objArray.join("")
+  let app_secrect = 'K1vX5aeNEMdnOctoUIzZpLDJXMf3IfPAv8TRNcJjmndq2TKisdX09FWr7'
+  console.log(sign + app_secrect)
+  sign = m.md5(sign + app_secrect).toUpperCase()
+  return sign
+}
 
 //通用post
 export const api_post = (option) => {
   // var baseURL = ''; //生产环境/开发、测试时打包时路径(变量写在全局url.js中在index.html中引入)
-  option.data.app_key = '735F59558122F70A35670715E8D27055'//需要传的app_key
   var option = {
     url: option.url || '',
     data: option.data || {},
@@ -13,6 +57,8 @@ export const api_post = (option) => {
     //请求未响应时的处理
     errorback: option.errorback || function () {}
   }
+  option.data.app_key = '735F59558122F70A35670715E8D27055'//需要传的app_key
+  // option.data.sign = toSign(option.data) // sign签名：后台设置已不需要签名
   return axios({
     method: 'post', //方法
     url: option.url, //地址
